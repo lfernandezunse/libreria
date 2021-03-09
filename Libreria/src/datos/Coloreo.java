c642dd8b399d3015faf2a5b3efd4a5b310bf2126
Lucas <lfernandez.unse@gmail.com>
2021-03-09 11:51:13 -0300

    me siento programadora

--- Libreria/src/datos/Coloreo.java
+++ Libreria/src/datos/Coloreo.java
@@ -10,5 +10,21 @@ package datos;
  * @author Lucas
  */
 public class Coloreo {
+    private int dificultad;
+
+    public Coloreo() {
+        this.dificultad = 0;
+    }
     
+    public Coloreo(int dificultad) {
+        this.dificultad = dificultad;
+    }      
+
+    public int getDificultad() {
+        return dificultad;
+    }
+
+    public void setDificultad(int dificultad) {
+        this.dificultad = dificultad;
+    }
 }
